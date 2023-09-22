dfdbe44d291cdf0b5c6f35c70f6907390c2c75c7
nijanthinisubramaniyan <145753561+nijanthinisubramaniyan@users.noreply.github.com>
2023-09-22 19:37:23 +0530

    Add files via upload
    odd num

---
+++ oddnum.java
@@ -0,0 +1,36 @@
+package Testll;
+
+public class oddnum {
+	
+	public static void main(String[] args) {
+		
+		
+		int n= 10;
+		System.out.println("odd num are");
+		for(int i=1;i<=n;i++)
+		
+		{
+			
+			if(i%2==1)
+			{
+				
+				System.out.println(i);
+				
+			}
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
